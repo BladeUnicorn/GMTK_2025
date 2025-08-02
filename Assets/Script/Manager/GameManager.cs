96a908102ec3014c459f7408fcfd43a32fd032a3
maomaoovo <maomaoovo@outlook.com>
2025-08-02 13:16:41 +0800

    feat: GameManager

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+namespace Script
+{
+    public enum GameState
+    {
+        GenerateGrid = 0,
+        SpawnPlayer = 1,
+    }
+    
+    public class GameManager : SingletonBase<GameManager>
+    {
+        private GameState currentGameState;
+
+        private void Start()
+        {
+            ChangeState(GameState.GenerateGrid);
+        }
+
+        public void ChangeState(GameState _newState)
+        {
+            currentGameState = _newState;
+            switch (_newState)
+            {
+                case GameState.GenerateGrid:
+                    GridManager.instance.GenerateGrid();
+                    break;
+                case GameState.SpawnPlayer:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(_newState), _newState,
+                        "Argument out of range exception");
+            }
+        }
+        
+    }
+}
